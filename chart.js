9a6c032c86d1e00b37f819674c8671490de2ff4a
Christian Adler <chris.adler@gmx.de>
2024-05-04 17:27:33 +0200

    add optional height for chart

--- chart.js
+++ chart.js
@@ -9,7 +9,7 @@ class Chart {
 
     this.canvas = document.createElement('canvas');
     this.canvas.width = options.size;
-    this.canvas.height = options.size;
+    this.canvas.height = options.height || options.size;
     this.canvas.style.backgroundColor = 'white';
     container.appendChild(this.canvas);
 
